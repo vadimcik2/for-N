0b8b33ddd550c0577f3e7f93c1415950197897c3
Vadim Caraan <vadimcaraman75@gmail.com>
2025-04-18 08:57:52 +0300

    AMUIA AM FACUT MODIFICARI

--- nata.py
+++
@@ -1,6 +0,0 @@
-# Acesta este un exemplu simplu de script Python
-def tema_python():
-    return "Tema Python a fost rulată cu succes!"
-
-if __name__ == "__main__":
-    print(tema_python())
--- nnn.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('runPython').addEventListener('click', function() {
-    // Aici ar trebui să adaugi logica pentru a rula scriptul Python
-    // De exemplu, poți folosi un API pentru a rula scriptul pe server
-    document.getElementById('output').innerText = "Tema Python a fost rulată!";
-});
